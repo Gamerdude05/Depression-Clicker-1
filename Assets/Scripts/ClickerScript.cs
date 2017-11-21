98a4b69a3724c58980cd601fdb7d3ec2e6cb8402
Gamerdude05 <gamerdude693@gmail.com>
2017-11-21 00:48:51 -0600

    Initial commit

---
+++ Assets/Scripts/ClickerScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickerScript : MonoBehaviour {
+
+    public UnityEngine.UI.Text DepressionDisplay;
+
+    public int depression = 0;
+
+    public int depressionperclick = 1;
+
+
+    void Update()
+    {
+        DepressionDisplay.text = "Depression: " + depression;
+        
+
+    }
+
+
+    public void Clicked()
+    {
+        depression += depressionperclick;
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
